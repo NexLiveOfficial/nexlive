7105ddfc865d994d169b804ecb45a5963a3bc16e
NexLiveOfficial <ricardo@nexliveapp.com>
2025-11-15 15:57:36 -0500

    changes to index.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,7 @@
 // pages/dashboard.js
 
 import { useEffect, useState } from 'react';
-import { supabase } from '../lib/supabaseClient';
+import supabase from './lib/supabaseClient';
 
 export default function Dashboard() {
   const [userEmail, setUserEmail] = useState('');
